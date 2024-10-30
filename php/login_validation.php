f6f3765d583ba5af4495d0ac9d7d8bc1c70632d9
koarine <koarine@gmail.com>
2024-10-30 22:51:22 +0800

    Add files via upload

--- php/login_validation.php
+++ php/login_validation.php
@@ -11,11 +11,12 @@
     $stmt=$db->query($query);
     $hashed_password=$stmt->fetch_assoc()['user_password'];
     if (password_verify($password,$hashed_password)){
-        $query="SELECT user_type,user_name FROM users WHERE username ='$username'";
+        $query="SELECT user_type,user_name,user_id FROM users WHERE username ='$username'";
         $stmt=$db->query($query);
         $val=$stmt->fetch_assoc();
         $_SESSION['user_type']=$val['user_type'];
         $_SESSION['user_name']=$val['user_name'];
+        $_SESSION['user_id']=$val['user_id'];
         header("Location: ../appointments.php");
         exit();
     }
