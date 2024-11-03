e68d1ae4239cd09c7ae4d62fc22290aaac9531ab
Omar <105975781+omar-ntu@users.noreply.github.com>
2024-11-03 16:59:22 +0800

    email + appt details

--- php/cancel.php
+++ php/cancel.php
@@ -12,8 +12,19 @@
         die("Connection failed: " . $db->connect_error);
     }   
     // get patient ID
-    $stmt = $db->query("SELECT PatientID FROM appointmentslots WHERE SlotID=$appt_id")->fetch_assoc();
+    $stmt = $db->query("SELECT * FROM appointmentslots WHERE SlotID=$appt_id")->fetch_assoc();
     $PatientID = $stmt['PatientID'];
+    $DoctorID = $stmt['DoctorID'];
+    $ApptDate = $stmt['AppointmentDate'];
+    $StartTime = $stmt['StartTime'];
+    $EndTime = $stmt['EndTime'];
+    $ConsultType = $stmt['ConsultType'];
+    $Comments = $stmt['Comments'];
+    
+    // get doctor name and clinic location
+    $stmt = $db->query("SELECT * FROM locations WHERE user_id=$DoctorID")->fetch_assoc();
+    $DoctorName = $stmt['doctor_name'];
+    $ClinicLocation = $stmt['clinic'];
 
     // update appointment slots for cancellation
     $query="UPDATE appointmentslots SET IsBooked=0,PatientID=NULL,ConsultType=NULL,Comments=NULL WHERE SlotID=$appt_id";
@@ -31,6 +42,13 @@
     // email message
     $message = "Dear $name,\n\n".
             "Your appointment with Radiant Smiles Dental has been cancelled.\n\n".
+            "Appointment Details:\n".
+            "Clinic Location: $ClinicLocation \n".
+            "Doctor: $DoctorName \n".
+            "Date: $ApptDate \n".
+            "Time: $StartTime to $EndTime \n".
+            "Consult Type: $ConsultType \n".
+            "Comments: $Comments \n\n".
             "Thank you for choosing Radiant Smiles Dental!\n\nBest regards,\nThe Radiant Smiles Dental Team";
 
     $headers = 'From: booking@radiantsmilesdental.com.sg'."\r\n".'Reply-To: booking@radiantsmilesdental.com.sg'."\r\n".'X-Mailer: PHP/'.phpversion();
