de652adedfba3110034c86256d1a78d67e9c6f91
Omar <105975781+omar-ntu@users.noreply.github.com>
2024-11-04 22:54:26 +0800

    changed contact.html to contact.php

--- appointments_doctor_view.php
+++ appointments_doctor_view.php
@@ -278,7 +278,7 @@
                 <div id="mid-header">
                     <a href="services.html" style="margin-left:2%">OUR SERVICES</a>   
                     <a href="about.html">ABOUT US</a>
-                    <a href="contact.html">CONTACT US</a>
+                    <a href="contact.php">CONTACT US</a>
                 </div>
                 <div id="right-header">
                 <a href="login.php">APPOINTMENTS</a>
