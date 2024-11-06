a6b61ae95f7949fa21de3b445f806d22ce225819
Omar <105975781+omar-ntu@users.noreply.github.com>
2024-11-06 13:28:19 +0800

    Delete mysql/dbcreate.sql

--- mysql/dbcreate.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE users (
-  user_id INT AUTO_INCREMENT PRIMARY KEY,
-  username VARCHAR(50) NOT NULL UNIQUE,
-  user_password varchar(255) NOT NULL,
-  user_type enum('doctor','patient') NOT NULL,
-  user_name varchar(100) NOT NULL,
-  email varchar(100) NOT NULL
-);
